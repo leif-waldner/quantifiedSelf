3b88785839f6a3c83d3a08e4e11c31a6a7c8e0d7
tree-waldo <leif.waldner@colorado.edu>
2015-12-02 21:51:42 -0700

    basic personality test site

---
+++ test-personality/public/src/button_test.js
@@ -0,0 +1,43 @@
+function question_change(state) {
+    var data = [
+        {id: 1, text: "This is one comment"},
+        {id: 2, text: "This is *another* comment"}
+    ];
+    document.getElementById('question').innerHTML = data[state].text;
+}
+
+function social_media_change(state) {
+    var data2 = [
+        {id: 1, text: "This is one comment aaa"},
+        {id: 2, text: "This is *another* comment aaa"}
+    ];
+    document.getElementById('social').innerHTML = data2[state].text;
+}
+
+var Button_Click = React.createClass({
+    getInitialState: function() {
+        return {count: 0};
+},
+onClick: function() {
+    social_media_change(this.state.count);
+    question_change(this.state.count);
+    this.state.count += 1;
+},
+render: function() {
+    return (
+        <div>
+            <button id="Strongly Agree" onClick={this.onClick}>Strongly Agree</button>
+            <button id="Agree" onClick={this.onClick}>Agree</button>
+            <button id="No Opinion" onClick={this.onClick}>No Opinion</button>
+            <button id="Disagree" onClick={this.onClick}>Disagree</button>
+            <button id="Strongly Disagree" onClick={this.onClick}>Strongly Disagree</button>
+        </div>
+    )
+}
+});
+
+ReactDOM.render(
+    <Button_Click />,
+    document.getElementById('buttons')
+);
+
