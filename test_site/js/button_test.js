01bef082fb42d8b43dae599514143a1c8436ce7c
tree-waldo <leif.waldner@colorado.edu>
2015-12-18 13:41:29 -0700

    cleaned up repo

--- test_site/js/button_test.js
+++ test_site/js/button_test.js
@@ -4,7 +4,6 @@ function question_change(index) {
         {id: 2, text: "This is *another* comment"}
     ];
     document.getElementById('question').innerHTML = data[index].text;
-    console.log(index);
 }
 
 function social_media_change(state) {
@@ -19,6 +18,12 @@ var Button_Click = React.createClass({
     getInitialState: function() {
         return {button_data: [], count: 0};
     },
+    handleHover: function() {
+        document.getElementById('social').style.visibility = 'visible';
+    },
+    handleMouseLeave: function() {
+        document.getElementById('social').style.visibility = 'hidden';
+    },
     handleClick: function(id) {
         this.state.button_data.push({"id": id, "question": question_change(this.state.count)});
         social_media_change(this.state.count);
@@ -28,7 +33,7 @@ var Button_Click = React.createClass({
     render: function() {
         return (
             <div>
-                <button className="btn" id="Strongly Agree" onClick={this.handleClick.bind(this, 1)}>Strongly Agree</button>
+                <button className="btn" id="Strongly Agree" onMouseEnter={this.handleHover} onMouseLeave={this.handleMouseLeave} onClick={this.handleClick.bind(this, 1)}>Strongly Agree</button>
                 <button className="btn" id="Agree" onClick={this.handleClick.bind(this, 2)}>Agree</button>
                 <button className="btn" id="No Opinion" onClick={this.handleClick.bind(this, 3)}>No Opinion</button>
                 <button className="btn" id="Disagree" onClick={this.handleClick.bind(this, 4)}>Disagree</button>
